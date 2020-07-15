0595b6a476c608f15c1a72b0cdfad1c3f2366ee9
Shimkek <maksim.dunaitsev@gmail.com>
2020-07-15 21:27:55 +0600

    added picsum.photos's pictures

---
+++ src/index.js
@@ -0,0 +1,29 @@
+var picQuantity = 6;
+
+const addPoster = () => {
+    picQuantity++;
+    const gallery = document.getElementById('gallery');
+    const gallery__poster = document.createElement('div');
+    gallery__poster.className = "gallery__poster";
+    const gallery__img = document.createElement('img');
+    gallery__img.className = "gallery__img";
+    const picSrc = 'https://picsum.photos/1920/1080?random='+ picQuantity;
+    gallery__img.setAttribute("src", picSrc);
+    gallery__poster.appendChild(gallery__img);
+    gallery.appendChild(gallery__poster);
+}
+
+const loadMore = () => {
+    for (i = 0; i < 6; i++) {
+        addPoster();
+      }
+}
+
+document.getElementById("loadMoreButton").addEventListener("click", function(){
+    loadMore();
+  });
+
+
+//         <div class="gallery__poster">
+//         <img class="gallery__img" src="https://picsum.photos/1920/1080">
+//         </div>
--- src/scripts.js
+++ src/scripts.js
@@ -2,7 +2,16 @@ const addPoster = () => {
     const gallery = document.getElementById('gallery');
     const gallery__poster = document.createElement('div');
     gallery__poster.className = "gallery__poster";
+    const gallery__img = document.createElement('img');
+    gallery__img.className = "gallery__img";
+    gallery__poster.appendChild(gallery__img);
     gallery.appendChild(gallery__poster);
+}
+
+const loadMore = () => {
+    for (i = 0; i < 6; i++) {
+        addPoster();
+      }
 }
 
 //         <div class="gallery__poster">
