17a0553bd17d7aba7bfea105f3b88df0baf8290e
Shimkek <maksim.dunaitsev@gmail.com>
2020-07-18 22:04:31 +0600

    added download button functunality

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 var picQuantity = 6;
+var isMaximized = false;
 
 const onclick = (id) => {
     document.getElementById(id).addEventListener('click', function () {
@@ -11,14 +12,14 @@ const addPoster = () => {
     const gallery = document.getElementById('gallery');
     const gallery__poster = document.createElement('div');
     gallery__poster.className = "gallery__poster";
-    gallery__poster.id = picQuantity;
     const gallery__img = document.createElement('img');
     gallery__img.className = "gallery__img";
     const picSrc = 'https://picsum.photos/1920/1080?random=' + picQuantity + '.jpg';
     gallery__img.setAttribute("src", picSrc);
+    gallery__img.id = picQuantity;
     gallery__poster.appendChild(gallery__img);
     gallery.appendChild(gallery__poster);
-  //  onclick(picQuantity); doesn't work with random photos
+    onclick(picQuantity); //doesn't work with random photos
 }
 
 const loadMore = () => {
@@ -43,15 +44,43 @@ const maximizeInterface = () => {
 
 const showMaximized = (pictureID) => {
     minimizeInterface();
-    const imgSrc = "https://picsum.photos/1920/1080?random=" + pictureID;
-    document.getElementById('maximized__img').setAttribute('src', imgSrc);
+   // const imgSrc = "https://picsum.photos/1920/1080?random=" + pictureID;
+   // document.getElementById('maximized__img').setAttribute('src', imgSrc);
+   if (isMaximized === true) {
+    const currentImg = document.getElementById('maximized__img');
+    currentImg.remove();
+   };
+    const clone = document.getElementById(pictureID).cloneNode(true);
+    clone.className = 'maximized__img';
+    clone.id='maximized__img';
     document.getElementById('maximized').style.visibility='visible';
+    document.getElementById('maximized__img-holder').appendChild(clone);
+    isMaximized = true;
 }
 
 document.getElementById('loadMoreButton').addEventListener('click', function () {
     loadMore();
 });
 
+document.getElementById('download-button').addEventListener('click', function () {
+    const pic = document.getElementById('maximized__img').cloneNode(true);
+    pic.id='';
+    const img_container = document.createElement('div');
+    img_container.className = 'original';
+    img_container.id='original';
+    pic.className= 'original__pic';
+    img_container.appendChild(pic);
+    const button = document.createElement('button');
+    button.className= 'original__button';
+    button.id="original__button";
+    const cancelSymbol = document.createTextNode('×');
+    button.appendChild(cancelSymbol);
+    img_container.appendChild(button);
+    document.body.appendChild(img_container);
+    document.getElementById('original__button').addEventListener('click', function () {
+        document.getElementById('original').remove();
+    });
+});
 
 document.getElementById(1).addEventListener('click', function () {
     showMaximized(1);
@@ -77,4 +106,4 @@ document.getElementById('close-button').addEventListener('click', function () {
 });
 
 
-
+//             <img class= "maximized__img" id="maximized__img" src=""> 
